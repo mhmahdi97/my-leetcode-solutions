2ddbdf19db83575e9a4ded5712f3952d09b7310c
mhmahdi97 <mhmahdi97@gmail.com>
2024-01-03 00:09:25 +0600

    solved problem 191 with c++

--- 1281.js
+++ 1281.js
@@ -1,17 +1,16 @@
 /* 
+1281. Subtract the Product and Sum of Digits of an Integer.
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
  
-
 Example 1:
-
 Input: n = 234
 Output: 15 
 Explanation: 
 Product of digits = 2 * 3 * 4 = 24 
 Sum of digits = 2 + 3 + 4 = 9 
 Result = 24 - 9 = 15
-Example 2:
 
+Example 2:
 Input: n = 4421
 Output: 21
 Explanation: 
@@ -21,7 +20,6 @@ Result = 32 - 11 = 21
 
 Constraints:
 1 <= n <= 10^5
-
 */
 function subtractProductAndSum(n) {
   let sum = 0;
